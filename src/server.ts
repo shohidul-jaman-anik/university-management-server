08f2d63f7474b8176835b55492feba25d7d662b4
Md Anik Hossain <anikh499@gmail.com>
2023-06-02 01:26:20 +0600

    Project Basic Setup Done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app"
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+      })      
+    console.log(`Database is Connected Successfully`);
+  } catch (error) {
+    console.log(`Failed to connect Database ${error}`);
+  }
+}
+
+main();
